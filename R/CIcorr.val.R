ced1ca84e64e4e2443bc6b79fa85b8b465a06bc8
TIAGO OLIVOTO <tiagoolivoto@gmail.com>
2018-06-04 17:05:14 -0300

    Intervalo de confiança não paramétrico do coeficiente de correlação de Pearson

--- R/CIcorr.val.R
+++ R/CIcorr.val.R
@@ -2,11 +2,11 @@ CIcorr.val = function(r, n) {
   CI=(0.45304^r)*2.25152*(n^-0.50089)
   UP=r+CI
   LP=r-CI
-  CIr=winDialog(type = "ok", paste0("Probability level: 5%","\n",
+  cat("\n")
+  cat(paste0("Probability level: 5%","\n",
                                     "Correlation coefficient: ", r,
                                     "\nSample size: ", n,
                                     "\nConfidence interval: ", round(CI,4),
                                     "\nTrue parameter range from: ", round(LP,4)," to ",
                                     round(UP,4)))
-  return(CIr)
 }
