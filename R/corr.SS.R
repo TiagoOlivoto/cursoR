31497ee173bb63a0e4eaaca35473d605483a2d94
TIAGO OLIVOTO <tiagoolivoto@gmail.com>
2018-06-04 17:05:42 -0300

    Planejamento do tamanho amostral para o coeficiente de correlação de Pearson

--- R/corr.SS.R
+++ R/corr.SS.R
@@ -1,8 +1,8 @@
 corr.SS = function(r, CI) {
   n=(CI/(0.45304^r*2.25152))^(1/-0.50089)
-  sampSize=winDialog(type = "ok", paste0("Probability level: 5%",
+  cat("\n")
+  cat(paste0("Probability level: 5%",
                                          "\nCorrelation coefficient: ", r,
                                          "\nHalf-width CI: ", CI,
                                          "\nRequired sample size: ", round(n,0)))
-  return(sampSize)
-}
+  }
