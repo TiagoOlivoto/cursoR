c8666816e84f46e439b6852998e82b5446c38cd5
TIAGO OLIVOTO <tiagoolivoto@gmail.com>
2018-06-24 20:35:36 -0300

    Computa diversas estatisticas relacionadas a interação genotipo vs ambiente

--- R/ge_stats.R
+++ R/ge_stats.R
@@ -70,14 +70,7 @@ ge_stats = function (data,
   d = aov(Yield ~ Rep%in%Env + Gen + Env + Gen*Env, data = data)
   anovaconj = anova(d)
   
-  ER =
-    suppressWarnings(stability::er_anova(
-      .data  = data,
-      .y = Yield,
-      .rep = Rep,
-      .gen  = Gen,
-      .env  = Env))
-  
+
   stab_meas =    
     suppressWarnings(stability::stab_measures(
       .data  = data,
@@ -85,25 +78,128 @@ ge_stats = function (data,
       .gen  = Gen,
       .env  = Env))
   
+  stab_meas$StabMeasures$GenSS = stab_meas$StabMeasures$GenSS * NR
+  stab_meas$StabMeasures$Var = stab_meas$StabMeasures$Var * NR
   stab_meas$StabMeasures$Ecov = stab_meas$StabMeasures$Ecov * NR
   stab_meas$StabMeasures = dplyr::mutate(stab_meas$StabMeasures, 
                                           Ecov.perc = Ecov/sum(Ecov)*100)
+
+  ########## ER ######
+  model1 <- lm(Yield ~ Gen + Env + Env/Rep + 
+                 Env * Gen, data = data)
+  modav <- anova(model1)
+  mydf = data.frame(aggregate(Yield ~ Gen + Env, 
+                              data = data, mean))
+  myAgg = aggregate(Yield ~ Gen, mydf, "c")
+  iamb = data.frame(aggregate(Yield ~ Env, 
+                              data = data, mean))
   
-  stab_reg = 
+  iamb = dplyr::mutate(iamb, 
+                       IndAmb = Yield -mean(Yield))
+  
+  iamb2 = data.frame(aggregate(Yield ~ Env + Gen, 
+                               data = data, mean))
+  
+  iamb2 = suppressMessages(dplyr::mutate(iamb2, 
+                        IndAmb = dplyr::left_join(iamb2, iamb %>% select(Env, IndAmb))$IndAmb))
+  
+  library(ggplot2)
+  library(plotly)
+  
+p = ggplot(iamb2, aes(x = IndAmb, y = Yield))+
+    geom_point(aes(colour = factor(Gen)), size = 1.5)+
+    geom_smooth(aes(colour = factor(Gen)), method = "lm", se = FALSE)+
+    theme_bw()+
+    labs(x = "Índice ambiental", y = "Produtividade de grãos")+
+    ggplot2::theme(axis.ticks.length = unit(.2, "cm"),
+                   axis.text = element_text(size = 12, colour = "black"),
+                   axis.title = element_text(size = 12, colour = "black"),
+                   axis.ticks = element_line(colour = "black"),
+                   plot.margin = margin(0.5, 0.5, 0.2, 0.6, "cm"),
+                   axis.title.y = element_text(margin = margin(r=16)),
+                   legend.title = element_blank(),
+                   legend.text = element_text(size=12),
+                   panel.border = element_rect(colour = "black", fill=NA, size=1),
+                   panel.grid.major.x = element_blank(), panel.grid.major.y = element_blank(),
+                   panel.grid.minor.x = element_blank(), panel.grid.minor.y = element_blank())
+  p2 = suppressMessages(ggplotly(p))
+  
+  matx <- myAgg$Yield
+  myAgg$Gen
+  meandf = data.frame(Gen = myAgg$Gen, myAgg$Yield)
+  names(meandf) = c("Gen", levels(mydf$Env))
+  gradyt = mean(matx)
+  iij = apply(matx, 2, mean) - gradyt
+  sqiij = sum((iij)^2)
+  YiIj = matx %*% iij
+  bij = YiIj/sqiij
+  svar = (apply(matx^2, 1, sum)) - (((apply(matx, 1, sum))^2)/ncol(matx))
+  bYijIj = bij * YiIj
+  deltaij = svar - bYijIj
+  devtab <- data.frame(Gen = meandf$Gen, svar, 
+                       bij, YiIj, bYijIj, deltaij)
+  S2e = modav$"Mean Sq"[5]
+  rps = length(levels(data$Rep))
+  en = length(levels(data$Env))
+  S2di = (deltaij/(en - 2)) - (S2e/rps)
+  meandf1 = data.frame(reshape::melt(meandf, id.var = "Gen"))
+  model2 <- lm(value ~ Gen + variable, data = meandf1)
+  amod2 <- anova(model2)
+  SSL = amod2$"Sum Sq"[2]
+  SSGxL = amod2$"Sum Sq"[3]
+  SS.L.GxL = SSL + SSGxL
+  SSL.Linear = (1/length(levels(data$Gen))) * (colSums(matx) %*% 
+                                                      iij)^2/sum(iij^2)
+  SS.L.GxL.linear = sum(bYijIj) - SSL.Linear
+  rps = rps
+  en = en
+  ge = length(levels(mydf$Gen))
+  Df <- c(en * ge - 1, ge - 1, ge * (en - 1), 1, ge - 1, ge * 
+            (en - 2), rep(en - 2, length(deltaij)), en * ge * (rps - 1))
+  poolerr = modav$"Sum Sq"[5]/rps
+  SSS <- c(sum(amod2$"Sum Sq"), amod2$"Sum Sq"[1], SSL + SSGxL, 
+           SSL.Linear, SS.L.GxL.linear, sum(deltaij), deltaij, poolerr) * rps
+  MSSS = (SSS/Df)
+  FVAL = c(NA, MSSS[2]/MSSS[6], NA, NA, MSSS[5]/MSSS[6], NA, 
+           MSSS[7:(length(MSSS) - 1)]/MSSS[length(MSSS)], NA)
+  PLINES = 1 - pf(FVAL[7:(length(MSSS) - 1)], Df[7], Df[length(Df)])
+  pval = c(NA, 1 - pf(FVAL[2], Df[2], Df[6]), NA, NA, 1 - pf(FVAL[5], 
+                                                             Df[5], Df[6]), NA, PLINES, NA)
+  anovadf <- data.frame(Df, `Sum Sq` = SSS, `Mean Sq` = MSSS, 
+                        `F value` = FVAL, `Pr(>F)` = pval, check.names = FALSE)
+  rownames(anovadf) <- c("Total", "Gen", "Env + (Gen x Env)", 
+                         "Env (linear)", " Gen x Env(linear)", "Pooled deviation", 
+                         levels(data$Gen), "Pooled error")
+  class(anovadf) <- c("anova", "data.frame")
+  name.y <- resp
+  
+  outdat = data.frame(Gen = devtab$Gen, bij = devtab$bij, 
+                      sdij = S2di)
+  ###############################################
+  ERREGR = 
     suppressWarnings(stability::stab_reg(
       .data  = data,
       .y = Yield,
       .rep = Rep,
       .gen  = Gen,
-      .env  = Env))
+      .env  = Env))[["StabIndvReg"]]
+
+  ERREGR$SSE =  ERREGR$SSE * NR
+  ERREGR$Delta =  ERREGR$Delta * NR
   
+  ERREGR$sdij = outdat$sdij
+  
+  ER = list(ANOVA = anovadf,
+            Regression = ERREGR)
   
   return(list(individual = individual,
               ge_means = means,
               anovaconj = anovaconj,
               ER = ER,
               stab_meas = stab_meas,
-              stab_reg = stab_reg))
+              iamb = iamb2,
+              plot = p,
+              iterplot = p2))
   
   
 }
