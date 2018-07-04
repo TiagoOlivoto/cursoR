df7e25d76fb00e952c86e7cc4b744adc4a8a8c68
TIAGO OLIVOTO <tiagoolivoto@gmail.com>
2018-07-04 15:07:08 -0300

    Gráficos de superfície de resposta

--- R/plot_supresp.R
+++ R/plot_supresp.R
@@ -1,24 +1,23 @@
-plot_supresp = function(data,
-                        type = NULL,
+plot_supresp = function(x,
+                        type,
                         col = NULL,
                         xlab = NULL,
                         ylab = NULL,
                         zlab = NULL,
                         contour = TRUE,
                         image = TRUE,
-                        facets = TRUE,
                         theta = 45,
                         d = 2, 
                         phi = 15,
                         leg.position = "rigth",
                         leg.width = 1,
-                        leg.length = 0.7,
+                        leg.length = 1,
                         leg.dist = 0,
                         leg.desl = 0,
                         export = FALSE,
                         file.type = "pdf",
                         file.name = NULL,
-                        width = 6,
+                        width = 7,
                         height = 6,
                         resolution = 300,
                         ...){
@@ -37,44 +36,47 @@ plot_supresp = function(data,
     side = 2
   }
   
-model = data$model
-par( mfrow = c(2, 2))
-if(type == "residuals"){
-plot(data$model)
-}
-par( mfrow = c(1, 1))
-data = data$results
+model = x$model
+x = x$results
 
-data3 = expand.grid(list(
-  A = seq(min(data[1]), max(data[1]), by = 1),
-  D = seq(min(data[2]), max(data[2]), by = 1)))
+x3 = expand.grid(list(
+  A = seq(min(x[1]), max(x[1]), by = 1),
+  D = seq(min(x[2]), max(x[2]), by = 1)))
 
-names(data3) = c(A, D)
+names(x3) = c(names(x)[1], names(x)[2])
 
-data3 = dplyr::mutate(data3, 
-                      predicted = predict(model, newdata = data3))
+x3 = dplyr::mutate(x3, 
+                      predicted = predict(model, newdata = x3))
 
 
 if (is.null(xlab) == T){
-  xlab = names(data[1])
+  xlab = names(x[1])
 }else {xlab = xlab}
 
 if (is.null(ylab) == T){
-  ylab = names(data[2])
+  ylab = names(x[2])
 }else {ylab = ylab}
 
 if (is.null(zlab) == T){
-  zlab = names(data[4])
+  zlab = names(x[4])
 }else {zlab = zlab}
 
-x = unique(data3[,1])
-y = unique(data3[,2])
-z = tapply(data3[, 3], data3[, c(1, 2)], mean)
+x = unique(x3[,1])
+y = unique(x3[,2])
+z = tapply(x3[, 3], x3[, c(1, 2)], mean)
 
+if(export == FALSE){
+
+  if(type == "residuals"){
+    par( mfrow = c(2, 2))
+    plot(model)
+    par( mfrow = c(1, 1))
+  }
+  
 if(type == "contourn"){
   plot3D::image2D(z = z,
-                  x = unique(data3[,1]),
-                  y = unique(data3[,2]),
+                  x = unique(x3[,1]),
+                  y = unique(x3[,2]),
                   contour = contour,
                   col = col,
                   xlab = xlab,
@@ -90,7 +92,7 @@ if(type == "surface"){
                   col = col,
                   clab = "", 
                   ticktype = "detailed",
-                  facets = facets,
+                  contour = contour,
                   image =  image,
                   theta = theta,
                   d = d, 
@@ -101,4 +103,98 @@ if(type == "surface"){
                   zlab = zlab,
                   ...)
 }
+}
+  if(export == TRUE){
+    
+    if (file.type == "pdf"){
+      if (is.null(file.name)){
+        pdf("Scatterplot Correlation.pdf",width = width, height = height)
+      } else
+        pdf(paste0(file.name, ".pdf"), width = width, height = height)
+      
+      if(type == "residuals"){
+        par( mfrow = c(2, 2))
+        plot(model)
+        par( mfrow = c(1, 1))
+      }
+      
+      if(type == "contourn"){
+        plot3D::image2D(z = z,
+                        x = unique(x3[,1]),
+                        y = unique(x3[,2]),
+                        contour = contour,
+                        col = col,
+                        xlab = xlab,
+                        colkey = list(length = leg.length, width = leg.width, side = side, dist = leg.dist, shift = leg.desl),
+                        ylab = ylab,
+                        ...)
+      }
+      
+      if(type == "surface"){
+        plot3D::persp3D(x = x, 
+                        y = y,
+                        z = z,
+                        col = col,
+                        clab = "", 
+                        ticktype = "detailed",
+                        contour = contour,
+                        image =  image,
+                        theta = theta,
+                        d = d, 
+                        phi = phi,
+                        colkey = list(length = leg.length, width = leg.width, side = side, dist = leg.dist, shift = leg.desl),
+                        xlab = xlab,
+                        ylab = ylab,
+                        zlab = zlab,
+                        ...)
+      }
+      
+      dev.off()
+    }
+    
+    if (file.type == "tiff"){
+      if (is.null(file.name)){
+        tiff(filename = "Scatterplot Correlation.tiff",width = width, height = height, units = "in", compression = "lzw", res = resolution)
+      } else
+        tiff(filename = paste0(file.name, ".tiff"), width = width, height = height, units = "in", compression = "lzw", res = resolution)
+      if(type == "residuals"){
+        par( mfrow = c(2, 2))
+        plot(model)
+        par( mfrow = c(1, 1))
+      }
+      
+      if(type == "contourn"){
+        plot3D::image2D(z = z,
+                        x = unique(x3[,1]),
+                        y = unique(x3[,2]),
+                        contour = contour,
+                        col = col,
+                        xlab = xlab,
+                        colkey = list(length = leg.length, width = leg.width, side = side, dist = leg.dist, shift = leg.desl),
+                        ylab = ylab,
+                        ...)
+      }
+      
+      if(type == "surface"){
+        plot3D::persp3D(x = x, 
+                        y = y,
+                        z = z,
+                        col = col,
+                        clab = "", 
+                        ticktype = "detailed",
+                        contour = contour,
+                        image =  image,
+                        theta = theta,
+                        d = d, 
+                        phi = phi,
+                        colkey = list(length = leg.length, width = leg.width, side = side, dist = leg.dist, shift = leg.desl),
+                        xlab = xlab,
+                        ylab = ylab,
+                        zlab = zlab,
+                        ...)
+      }
+      dev.off()
+    }
+    
+  }
 }
