74b3968734c4b9fb1f641a80334dd343d8b551da
TIAGO OLIVOTO <tiagoolivoto@gmail.com>
2018-07-06 15:12:01 -0300

    Coeficiente de correlação gáfico-numérico

--- R/corr.plot.R
+++ R/corr.plot.R
@@ -7,17 +7,21 @@ corr.plot = function(Dataset,
                      height = 7,
                      resolution = 300,
                      sizepoint = 0.5,
-                     minsize = 1.5,
-                     maxsize = 2.5,
-                     smooth = FALSE,
-                     signcol = "gray",
-                     alpha = 0.25,
-                     diagcol = "gray" ){
+                     minsize = 2,
+                     maxsize = 3,
+                     smooth = TRUE,
+                     signcol = "green",
+                     alpha = 0.15,
+                     diagcol = "gray",
+                     col.up.panel = "gray",
+                     col.lw.panel = "gray",
+                     col.dia.panel = "black",
+                     pan.spacing = 0.15){
 
 my_custom_cor = function(data, mapping, color = I("black"), sizeRange = c(minsize, maxsize), ...) {
     # get the x and y data to use the other code
-  x = eval(mapping$x, data)
-  y = eval(mapping$y, data)
+  x = GGally::eval_data_col(data, mapping$x)
+  y = GGally::eval_data_col(data, mapping$y)
   ct = cor.test(x,y)
   sig = symnum(
     ct$p.value, corr = FALSE, na = FALSE,
@@ -58,7 +62,7 @@ my_custom_cor = function(data, mapping, color = I("black"), sizeRange = c(minsiz
     # remove all the background stuff and wrap it with a dashed line
     theme_classic() +
     theme(
-      panel.background = element_rect(color = "gray50"),
+      panel.background = element_rect(color = col.up.panel),
       axis.line = element_blank(),
       axis.ticks = element_blank(),
       axis.text.y = element_blank(),
@@ -69,11 +73,20 @@ my_custom_cor = function(data, mapping, color = I("black"), sizeRange = c(minsiz
 ###############   Definindo diagonal inferior ###############
 
 my_custom_smooth = function(data, mapping, ...) {
+  x = GGally::eval_data_col(data, mapping$x)
+  y = GGally::eval_data_col(data, mapping$y)
+  
+  ct <- cor.test(x,y)
+  
+  r <- unname(ct$estimate)
+  rt <- format(r, digits=2)[1]
+  tt <- as.character(rt)
+  
   p = ggplot(data = data, mapping = mapping) +
     geom_point(color = I("black"), size = sizepoint) +
     theme_classic() +
     theme(
-      panel.background = element_rect(fill = "white", color = "gray50"),
+      panel.background = element_rect(fill = "white", color = col.lw.panel),
       axis.line = element_blank(),
       axis.ticks = element_blank(),
       axis.text.y = element_blank(),
@@ -85,7 +98,7 @@ my_custom_smooth = function(data, mapping, ...) {
     geom_smooth(method = "lm", size=0.3, color = I("red"), ...)+
     theme_classic() +
     theme(
-      panel.background = element_rect(fill = "white", color = "gray50"),
+      panel.background = element_rect(fill = "white", color = col.lw.panel),
       axis.line = element_blank(),
       axis.ticks = element_blank(),
       axis.text.y = element_blank(),
@@ -93,12 +106,7 @@ my_custom_smooth = function(data, mapping, ...) {
     )
     }
 
-  lmModel = eval(substitute(lm(y ~ x, data = data), mapping))
-  fs = summary(lmModel)$fstatistic
-  pValue = pf(fs[1], fs[2], fs[3], lower.tail = FALSE)
-
-
-  if (pValue < prob) {
+  if (r > prob) {
     p = p + theme(
       panel.background = element_rect(fill=alpha(signcol, alpha)))
 
@@ -113,18 +121,18 @@ ggally_mysmooth = function(data, mapping, ...){
   ggplot(data = data, mapping=mapping) +
     geom_density(fill=alpha(diagcol, 1))+
     theme_classic() +
-    theme(panel.background = element_rect(fill=alpha('white', 1), color = "gray"))
+    theme(panel.background = element_rect(fill=alpha('white', 1), color = col.dia.panel))
 }
 
 ################ Plot Correlation ##############
 
-p1=ggpairs(
+p1 = GGally::ggpairs(
   Dataset[,1:ncol(Dataset)],
   upper = list(continuous = my_custom_cor),
   lower = list(continuous = my_custom_smooth),
   diag = list(continuous = ggally_mysmooth),
   axisLabels="none")
-theme_set(theme_gray()+theme(panel.spacing=grid::unit(0.15,"lines")))
+theme_set(theme_gray()+theme(panel.spacing=grid::unit(pan.spacing,"lines")))
 
 if (export  ==  F|FALSE) {
   print(p1)
