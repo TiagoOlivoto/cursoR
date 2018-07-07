47f54254007c9959d51830e2fc166dbfc9687730
TIAGO OLIVOTO <tiagoolivoto@gmail.com>
2018-07-07 13:36:28 -0300

    Análise de trilha e seleção de variáveis para preditores com mínima multicolinearidade

--- R/path.coeff.R
+++ R/path.coeff.R
@@ -94,7 +94,7 @@ if (brutstepwise == FALSE){
       labs(x = "k values", y = expression(paste( beta, " values")))+
       geom_abline(intercept = 0, slope = 0)+
       scale_x_continuous(breaks=seq(0,1,by=0.1))
-    
+    iterplot = plotly::ggplotly(p1)
   } else {p1 = "No graphic generated due to correction value"}
 
   eigen = eigen(cor.x)
@@ -145,6 +145,7 @@ if (brutstepwise == FALSE){
                           Eigen = AvAvet,
                           VIF = VIF,
                           plot = p1,
+                          iterplot = itgraphic,
                           Predictors = pred,
                           CN = NC,
                           Det = Det,
