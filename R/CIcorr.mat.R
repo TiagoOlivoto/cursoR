c8b8fe2b6c666a7e8503bb46de3c5c807981e859
TIAGO OLIVOTO <tiagoolivoto@gmail.com>
2018-07-12 16:36:06 -0300

    correção no nome das linhas

--- R/CIcorr.mat.R
+++ R/CIcorr.mat.R
@@ -5,8 +5,7 @@ CIcorr.mat = function(data){
   Ncorr = (Ncolu*(Ncolu-1))/2
   Corconf = data.frame(type = matrix(ncol =  1, nrow = Ncorr))  
   names = colnames(data)
-  combnam = combn(names,2, paste, collapse = "x")
-  rownames(Corconf)=names(sapply(combnam, names))
+  combnam = combn(names,2, paste, collapse = " x ")
   names(Corconf) = "Corr"
   corr = cor(data)
   vector =  data.frame(t(corr)[lower.tri(corr,diag=F)])
@@ -16,6 +15,7 @@ CIcorr.mat = function(data){
                           CI = (0.45304^abs(Corr))*2.25152*(Nlinha^-0.50089),
                           LL = Corr - CI,
                           UL = Corr + CI)
+  rownames(Corconf)=names(sapply(combnam, names))
   return(Corconf)
                      
 }
