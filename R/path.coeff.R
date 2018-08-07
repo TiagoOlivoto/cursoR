9a3d84f17bdd07e7f8e98df54390fb39c11a7a33
TIAGO OLIVOTO <tiagoolivoto@gmail.com>
2018-08-07 16:55:17 -0300

    selected predictor message included

--- R/path.coeff.R
+++ R/path.coeff.R
@@ -189,10 +189,11 @@ if (brutstepwise == FALSE){
     nproced = npred - 1
     
     cat(paste("The brutestepwise algorithm have selected a set of ",nrow(VIF3),
-              "predictors with largest VIF = ", round(max(VIF3$VIF),3)),
-        ". Now, a stepwise regression procedure will fit ",nproced, " models.", "\n")
+              "predictors with largest VIF = ", round(max(VIF3$VIF),3)),".\n")
+    cat("Selected predictors:",paste0(selectedpred),"\n")
+    cat(paste("Now, a stepwise regression procedure will fit ",nproced, " models.", "\n"))
     
-    
+    selectedpred = path$Selectedpred
     for (i in 1:nproced){
       
     FDSel =  FWDselect::selection(x = xxx,
